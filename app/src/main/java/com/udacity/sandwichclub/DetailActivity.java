25628b177557c9e9403f5814b36cc88047b87c67
miro.jurisic <mirojurisic@hotmail.com>
2019-10-31 22:23:20 +0800

    added json parsing for sandwich added populating UI for each sandwich rearranged detailed layout for each of the sandwiches

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -43,7 +44,8 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +58,12 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+        ((TextView)  findViewById(R.id.description_tv)).setText(sandwich.getDescription());
+        ((TextView)  findViewById(R.id.ingredients_tv)).setText(sandwich.getIngredients().toString().replaceAll("[\\[\\]]" , ""));
+        ((TextView)  findViewById(R.id.origin_tv)).setText(sandwich.getPlaceOfOrigin());
+        ((TextView)  findViewById(R.id.also_known_tv)).setText(sandwich.getAlsoKnownAs().toString().replaceAll("[\\[\\]]" , ""));
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,64 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Log.v("Json_parsing",json);
+        String mainName="";
+        List<String> alsoKnownAs = new ArrayList<>();
+        String placeOfOrigin="";
+        String description="";
+        String image="";
+        List<String> ingredients = new ArrayList<>();
+        JSONArray ingredients1 = null;
+
+        try {
+            JSONObject sandwiches = new JSONObject(json);
+            JSONObject baseName = sandwiches.getJSONObject("name");
+            mainName = baseName.getString("mainName");
+            Log.v("Json_parsing",mainName);
+            JSONArray alsoKnownAs1 = baseName.getJSONArray("alsoKnownAs");
+            if(alsoKnownAs1.length()>0)
+            {
+                for (int i = 0; i<alsoKnownAs1.length();i++)
+                {
+                    alsoKnownAs.add(alsoKnownAs1.getString(i));
+                }
+            }
+            if(sandwiches.has("placeOfOrigin"))
+                placeOfOrigin = sandwiches.getString("placeOfOrigin");
+            if(sandwiches.has("description"))
+                description = sandwiches.getString("description");
+            Log.v("Json_parsing",description);
+
+            if(sandwiches.has("image"))
+                image = sandwiches.getString("image");
+            Log.v("Json_parsing",image);
+            if(sandwiches.has("ingredients"))
+                 ingredients1 = sandwiches.getJSONArray("ingredients");
+            if(!ingredients.isEmpty() && ingredients1.length()>0)
+            {
+                for (int i = 0; i<ingredients1.length();i++)
+                {
+                    ingredients.add(ingredients1.getString(i));
+                }
+            }
+        }
+        catch (org.json.JSONException e)
+        {
+            Log.e("JSON Error",e.toString());
+        }
+        return new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,image,ingredients);
     }
 }
