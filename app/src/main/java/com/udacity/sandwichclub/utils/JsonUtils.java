a686595c2bc4e51a7dccc985838606ab295b2af8
Miro.Jurisic <mjurisic86@gmail.com>
2019-11-01 09:43:32 +0800

    introduced string constants and fixed json parsing error with reading ingredients removed multiple linear layouts and replaced with single relative layout fixed with of the image to 120dp

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -11,23 +11,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
-
+    public static final String TAG = JsonUtils.class.getSimpleName();
+    private static final String KEY_MAIN_NAME = "mainName";
+    private static final String KEY_ALSO_KNOW_AS = "alsoKnownAs";
+    private static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String KEY_IMAGE= "image";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_INGREDIENTS = "ingredients";
     public static Sandwich parseSandwichJson(String json) {
 
-        Log.v("Json_parsing",json);
+        Log.v(TAG,json);
         String mainName="";
         List<String> alsoKnownAs = new ArrayList<>();
         String placeOfOrigin="";
         String description="";
         String image="";
         List<String> ingredients = new ArrayList<>();
-        JSONArray ingredients1 = null;
+        JSONArray ingredientsArray = null;
 
         try {
             JSONObject sandwiches = new JSONObject(json);
             JSONObject baseName = sandwiches.getJSONObject("name");
-            mainName = baseName.getString("mainName");
-            JSONArray alsoKnownAs1 = baseName.getJSONArray("alsoKnownAs");
+            mainName = baseName.getString(KEY_MAIN_NAME);
+            JSONArray alsoKnownAs1 = baseName.getJSONArray(KEY_ALSO_KNOW_AS);
             if(alsoKnownAs1.length()>0)
             {
                 for (int i = 0; i<alsoKnownAs1.length();i++)
@@ -35,26 +41,26 @@ public class JsonUtils {
                     alsoKnownAs.add(alsoKnownAs1.getString(i));
                 }
             }
-            if(sandwiches.has("placeOfOrigin"))
-                placeOfOrigin = sandwiches.getString("placeOfOrigin");
-            if(sandwiches.has("description"))
-                description = sandwiches.getString("description");
+            if(sandwiches.has(KEY_PLACE_OF_ORIGIN))
+                placeOfOrigin = sandwiches.getString(KEY_PLACE_OF_ORIGIN);
+            if(sandwiches.has(KEY_DESCRIPTION))
+                description = sandwiches.getString(KEY_DESCRIPTION);
 
-            if(sandwiches.has("image"))
-                image = sandwiches.getString("image");
-            if(sandwiches.has("ingredients"))
-                 ingredients1 = sandwiches.getJSONArray("ingredients");
-            if(!ingredients.isEmpty() && ingredients1.length()>0)
+            if(sandwiches.has(KEY_IMAGE))
+                image = sandwiches.getString(KEY_IMAGE);
+            if(sandwiches.has(KEY_INGREDIENTS))
+                 ingredientsArray = sandwiches.getJSONArray(KEY_INGREDIENTS);
+            if(!(ingredientsArray==null) && ingredientsArray.length()>0)
             {
-                for (int i = 0; i<ingredients1.length();i++)
+                for (int i = 0; i<ingredientsArray.length();i++)
                 {
-                    ingredients.add(ingredients1.getString(i));
+                    ingredients.add(ingredientsArray.getString(i));
                 }
             }
         }
         catch (org.json.JSONException e)
         {
-            Log.e("JSON Error",e.toString());
+            Log.e(TAG,e.toString());
         }
         return new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,image,ingredients);
     }
